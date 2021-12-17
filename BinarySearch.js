8942c6f92f753afe9ced14d4ea173b74ca85138d
Yash Shrivastava <yshrivastava@deloitte.com>
2021-12-17 21:44:05 +0530

    O(n^2) sorting and binary search

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,27 +1,31 @@
-function BinarySearch(a,ele){
+function binarySearch(a,ele)
+{
+    let n=a.length;
+    let first=0;let last=n-1;
+    let mid;
+    for(let i=0;i<n/2;i++)
+    {
+        mid=Math.floor((first+last)/2);
+        //console.log(a[mid]); Division in Js returns decimal value hecne floor function needs to be used to get the greatest integer less than or equal to the quotient 
+        if(a[mid]==ele)
+            { 
+                console.log(`${ele} is a at position ${mid+1}`);
+                break;
+            }
+        else if(a[mid]>ele)
+            last=mid-1;
+        else if(a[mid]<ele)
+            first=mid+1;
+        else console.log(`${ele} not found!`)
 
-    let size= a.length;
-    let first=0;let last=size-1;
-    for(let i=0;i<size;i++){// while last > first
-
-        middle=(first+last)/2;
-        
-        if(a[middle] == ele)
-            return middle + 1 ;
-        else if(ele > a[middle])
-            first = middle + 1;
-
-        else if(ele < a[middle])
-            last = middle - 1;
-
-        else return `${ele} not found!`;
-        
     }
-
 }
+const a=[1,2,3,4,5,6,7,8,9]
+console.log(binarySearch(a,9))
 
-let compare = (a,b) =>{
- return a-b;
-}
-
-console.log(BinarySearch([1,2,3,4,5,6,7,55,334,345,3333].sort(compare),7))
+//Comparison function
+// let compare = (a,b) =>{
+//     return a-b;
+//    }
+   
+//    console.log(BinarySearch([1,2,3,4,5,6,7,55,334,345,3333].sort(compare),7))
---
+++ sortingAlgorithms.js
@@ -0,0 +1,91 @@
+//O(n^2)
+function bubble(a)
+{
+    let n=a.length;
+    let temp;
+    for(let i=0;i<n-1;i++)
+        {
+            for(let j=i+1;j<n;j++)
+            {
+                console.log(a[i]+' '+a[j])
+                if(a[j]<a[i])
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        console.log(a);
+}
+
+function selection(a)
+{
+    let n=a.length;
+    let min,temp;
+    for(let i=0;i<n;i++)
+    {
+        min=i;
+        for(let j=i+1;j<n-1;j++)
+            {
+                if(a[j]<a[min])
+                    min=j;
+            }
+            temp=a[i];
+            a[i]=a[min];
+            a[min]=temp;
+     }   
+     console.log(a);
+}
+
+function insertion(a)
+{
+    let n=a.length;
+    let temp=0;
+    let temp2=0;
+    let flag=false;                                                    
+    for(let i=0;i<n;i++)
+    {
+        for(let j=0;j<=i;j++)
+        {
+            if(flag)
+            {
+                //Bubbling of elements with Right Shift 
+                temp2=a[j];
+                a[j]=temp;
+                temp=temp2;
+            }
+            if(a[j]>a[i] && flag==false)
+            {
+                // Insertion
+                temp=a[j];
+                a[j]=a[i];
+
+                flag=true;// Setting the domino effect
+            }
+            if(j==i)
+                flag=false;
+        }
+    
+    } 
+    console.log(a)  
+}
+//O(nlogn)
+// function merge(a)
+// {
+
+// }
+// function quick(a)
+// {
+
+// }
+// function heap(a)
+// {
+
+// }
+
+
+const a=[5,2,6,3,8,9,7,1,4];
+bubble(a)
+selection(a)
+insertion(a)
