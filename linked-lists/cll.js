5979dd9b377be096d816159854c3162d99824790
Yash Shrivastava <yshrivastava@deloitte.com>
2022-01-28 20:24:52 +0530

    reorganized folders

---
+++ linked-lists/cll.js
@@ -0,0 +1,49 @@
+function node(value){
+    this.data=value;
+    this.next=null;
+}
+
+class CircularLinkedList{
+    constructor(){
+        this.head=null;
+        this.size=0;
+    }
+    traverse(){// rem=(a[i]%size) 
+        let current=this.head;
+        while(current!=head){//how will ya start??
+
+        }
+    }
+    insertFirst(){
+
+    }
+    insertLast(){
+
+    }
+    insertAtIndex(){
+
+    }
+    reverse(){
+
+    }
+
+   // deleteFirst=()=>{
+
+    // }
+
+
+    // deleteLast=()=>{
+
+    // }
+
+
+    // deleteAtIndex=()=>{
+
+    // }
+
+
+    // sort=()=>{
+
+    // }
+    
+}
